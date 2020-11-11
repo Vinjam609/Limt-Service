1dde35576d255f7959405be3c9f11378583d20db
unknown <RAMM@view-localhost>
2020-11-11 12:20:07 +0530

    Additional Comments added for more clarity

--- limit-service/src/main/java/com/microservices/limitservice/limits/LimitConfigurationController.java
+++ limit-service/src/main/java/com/microservices/limitservice/limits/LimitConfigurationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitConfigurationController {
 	
+//Aded some comments for more calrification
 	@Autowired
 	private LimitsApplPropConfiguration limitValues;
 
